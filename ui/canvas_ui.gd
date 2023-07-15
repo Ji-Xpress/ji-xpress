bdb62a084e5643656841b9380a0391be4009eff9
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-07-15 13:08:16 +0400

    Implement the Project Tree UI

---
+++ ui/canvas_ui.gd
@@ -0,0 +1,42 @@
+extends Control
+
+# Node references
+@onready var properties_editor: Control = $PanelContainer/HSplitContainer/RightPanel/ControlContainer/PropertiesEditor
+@onready var design_canvas: Node2D = $PanelContainer/HSplitContainer/LeftPanel/ControlContainer/SubViewportContainer/SubViewport/DesignCanvas
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Design canvas event handling
+
+func _on_design_canvas_node_hover(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+func _on_design_canvas_node_hover_out(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+func _on_design_canvas_node_selected(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+func _on_design_canvas_node_deselected(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+func _on_design_canvas_node_moved(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+func _on_design_canvas_node_rotated(node, node_index, node_kind):
+	pass # Replace with function body.
+
+
+# Properties editor event handling
+
+func _on_properties_editor_property_changed(property_set_id, property_id, new_value):
+	pass # Replace with function body.
