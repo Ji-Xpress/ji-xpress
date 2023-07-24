3e279e84092f1d9fbaafd4e82ebd87194bf7c89d
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-07-24 20:43:43 +0400

    Rearrange Main UI

--- data_models/project_metadata.gd
+++ data_models/project_metadata.gd
@@ -3,10 +3,12 @@ class_name ProjectMetadata
 
 # Constants
 const prop_app_version: String = "app_version"
+const prop_startup_scene: String = "startup_scene"
 
 
 ## Return the dict template
 static func model_template():
 	return {
-		prop_app_version: ""
+		prop_app_version: "",
+		prop_startup_scene: ""
 	}
--- main_ui.gd
+++ main_ui.gd
@@ -5,12 +5,32 @@ extends Control
 @onready var project_tree_ui: Control = $PanelContainer/VBoxContainer/HSplitContainer/ProjectTreeUI
 @onready var dialogs: Control = $Dialogs
 
+@onready var save_project_button: Button = $PanelContainer/VBoxContainer/Menu/MarginContainer/HBoxContainer/SaveProjectButton
+@onready var close_project_button: Button = $PanelContainer/VBoxContainer/Menu/MarginContainer/HBoxContainer/CloseProjectButton
+@onready var run_project_scene_button: Button = $PanelContainer/VBoxContainer/Menu/MarginContainer/HBoxContainer/RunProjectSceneButton
+@onready var run_project_button: Button = $PanelContainer/VBoxContainer/Menu/MarginContainer/HBoxContainer/RunProjectButton
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Save button has been pressed
-func _on_file_menu_button_save_pressed():
+# Perform project save
+func _on_save_project_button_pressed():
+	pass # Replace with function body.
+
+
+# Perform project close
+func _on_close_project_button_pressed():
+	pass # Replace with function body.
+
+
+# We need to run the current scene
+func _on_run_project_scene_button_pressed():
+	pass # Replace with function body.
+
+
+# We need to run the project
+func _on_run_project_button_pressed():
 	pass # Replace with function body.
