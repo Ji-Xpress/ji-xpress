fd2fe882514b8e4da48a2f4bd2e8e55ee33e1ffa
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-07-28 16:06:36 +0400

    Make a functional alien

--- game_objects/physics/objects/alien.gd
+++ game_objects/physics/objects/alien.gd
@@ -6,6 +6,9 @@ extends RigidBody2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var camera: Camera2D = $Camera2D
 
+# Tracks to see if we are on the floor
+var is_on_floor: bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +23,22 @@ func _ready():
 		mass = float(object_metadata.get_property("mass"))
 
 
+# Physics loop
+func _physics_process(delta):
+	if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
+		if is_on_floor:
+			if not Input.is_action_pressed("ui_accept"):
+				var force_raycast: Vector2 = get_left_right_input()
+				apply_central_force(Vector2(force_raycast.x * mass * 500, 0))
+			else:
+				apply_central_force(Vector2(0, mass * 10000 * -1))
+
+
+# Checks to see if keyboard is
+func get_left_right_input():
+	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+
 # Collision detection logic
 func _on_body_entered(body):
 	if body.is_in_group("explosive"):
@@ -27,3 +46,15 @@ func _on_body_entered(body):
 		var force_vector:Vector2 = Vector2(0, force * -1)
 		var object_position: Vector2 = body.position
 		apply_impulse(force_vector, object_position - position)
+
+
+func _on_floor_detector_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
+		if body.is_in_group("tile"):
+			is_on_floor = true
+
+
+func _on_floor_detector_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
+		if body.is_in_group("tile"):
+			is_on_floor = false
