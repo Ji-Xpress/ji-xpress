bc6fb44afc70b7874d5c89f748efd1f523c1be20
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-08-15 20:22:04 +0400

    Build the expression engine

--- project_src/code_execution/expression_engine.gd
+++ project_src/code_execution/expression_engine.gd
@@ -12,4 +12,38 @@ func initialize_engine():
 
 ## Evaluates an expression and sends back the results
 func compute_expression(expression: String):
-	return null
+	var var_names = []
+	var var_values = []
+	
+	# Build variable sets for the expression
+	if current_condition_type != "":
+		if SharedState.has(current_condition_type):
+			for variable in SharedState[current_condition_type]:
+				var_names.append(variable)
+				var_values.append(SharedState[current_condition_type][variable])
+	
+	var_names.append("variables")
+	var_values.append(game_object_instance.object_coder.variable_values)
+	var_names.append("globals")
+	var_values.append(SharedState.state_variables)
+	var_names.append("properties")
+	var_values.append(game_object_instance.object_metadata.prop_values)
+	
+	# Evaluate expression and return results
+	return evaluate(expression, var_names, var_values)
+
+
+## Evaluation of an expression
+func evaluate(command, variable_names = [], variable_values = []):
+	var expression = Expression.new()
+	var error = expression.parse(command, variable_names)
+	if error != OK:
+		push_error(expression.get_error_text())
+		return null
+
+	var result = expression.execute(variable_values, self)
+
+	if not expression.has_execute_failed():
+		return result
+	else:
+		return null
