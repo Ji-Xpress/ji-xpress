abd247e52959e3f2df84a339ccd830b5d662c387
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-08-17 12:48:16 +0400

    Implement the code execution block type: function

--- project_src/code_execution/block_types/function.gd
+++ project_src/code_execution/block_types/function.gd
@@ -3,7 +3,8 @@ extends BlockTypeExecutionBase
 
 ## Computes the result of the block's execution
 func compute_result():
-	return false
+	var condition_result = game_object_instance.call(sub_type, block_parameters)
+	return condition_result
 
 
 ## Checks to see if the block reverts back to a finally
