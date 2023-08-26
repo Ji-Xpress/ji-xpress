d182063c3a3770934b744c266c54fe69a0676635
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-08-26 15:38:50 +0400

    Add object coder to the background object

--- game_objects/physics/objects/background.gd
+++ game_objects/physics/objects/background.gd
@@ -3,3 +3,4 @@ extends Node2D
 # Node references
 @onready var object_metadata: ObjectMetaData = $ObjectMetaData
 @onready var object_functionality: ObjectFunctionality = $ObjectFunctionality
+@onready var object_coder: ObjectCoder = $ObjectCoder
