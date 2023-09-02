bab6e7ca35ae17dc00043d0ef068ec5ce7226176
Ahmed Maawy <ultimateprogramer@gmail.com>
2023-09-02 19:16:52 +0400

    Implement resize capabilities for moving platform

--- game_objects/platformer/objects/elements_moving_platform.gd
+++ game_objects/platformer/objects/elements_moving_platform.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const block_dimensions: Vector2 = Vector2(64, 64)
+
 const position_top: int = 0
 const position_bottom: int = 1
 const position_left: int = 0
@@ -24,16 +26,25 @@ var movement_duration: float = 1.0
 var current_vertical_position: int = 0
 var current_horizontal_position: int = 0
 
+var top_sprite: TileSpriteBase = null
+var rect_extents: RectExtents2D = null
+var current_width: int = block_dimensions.x
+var current_height: int = block_dimensions.y
+
 # Tween variavle
 var tween: Tween = null
 # Update loop code execution
 var update_code_execution_engine: CodeExecutionEngine = null
-
 var initial_global_position: Vector2 = Vector2.ZERO
+var current_collider_height: int = 0
 
 
 # Initialize
 func _ready():
+	# Initialize the object
+	top_sprite = get_node("Sprite2D")
+	rect_extents = get_node("RectExtents2D")
+	
 	if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
 		update_code_execution_engine = object_coder.code_execution_engine()
 		var code_execution_engine = object_coder.code_execution_engine()
@@ -50,7 +61,39 @@ func _ready():
 	initial_global_position = global_position
 	
 	activate()
+	invalidate_rect()
 	do_tween()
+
+
+## Invalidate the rect
+func invalidate_rect():
+	# Prepare variables
+	var width: int = current_width / block_dimensions.x
+	var height: int = current_height / block_dimensions.y
+	var extents_size: Vector2 = Vector2(current_width, current_height)
+	
+	# Set block parameters
+	top_sprite.set_block_width(width)
+	
+	# Reposition rect
+	rect_extents.position.x = current_width / 2 - (block_dimensions.x / 2)
+	rect_extents.position.y = current_height / 2 - (block_dimensions.y / 2)
+	rect_extents.size = extents_size
+	
+	if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeDesign:
+		# If in design mode change the size of the canvas hit area
+		var canvas_mouse_hit_area: Area2D = get_node("CanvasMouseHitArea")
+		
+		if canvas_mouse_hit_area != null:
+			canvas_mouse_hit_area.position.x = current_width / 2 - (block_dimensions.x / 2)
+			canvas_mouse_hit_area.position.y = current_height / 2 - (block_dimensions.y / 2)
+			canvas_mouse_hit_area.set_hit_size(extents_size, true)
+	elif object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
+		# If in run mode change the size of the actual collider
+		collision_shape.position.x = current_width / 2 - (block_dimensions.x / 2)
+		collision_shape.position.y = current_height / 2 - (block_dimensions.y / 2)
+		collision_shape.shape.size.x = extents_size.x
+		collision_shape.shape.size.y = current_collider_height
 
 
 ## Fill in variables from props
@@ -65,6 +108,9 @@ func fill_in_vars_from_props():
 		move_horizontally = str(object_metadata.get_property("move_horizontally")) == "true"
 		move_vertically = str(object_metadata.get_property("move_vertically")) == "true"
 		movement_duration = float(object_metadata.get_property("movement_duration"))
+	
+	current_width = int(object_metadata.get_property("width")) * block_dimensions.x
+	current_collider_height = collision_shape.shape.size.y
 
 
 ## Gets the movement vector based on settings
@@ -161,6 +207,10 @@ func _on_object_functionality_property_changed(property, value, is_custom):
 			if object_metadata.node_mode == SharedEnums.NodeCanvasMode.ModeRun:
 				tween.stop()
 				do_tween()
+		"width":
+			var width: int = int(value)
+			current_width = width * block_dimensions.x
+			invalidate_rect()
 
 
 # Process the broadcast message
