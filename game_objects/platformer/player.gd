8078074b336fdc46d94f185dedadf5fa81bfa11e
Ahmed Maawy <ultimateprogramer@gmail.com>
2024-01-25 13:58:22 +0400

    Implement platformer and physics pack standalone players

--- game_objects/platformer/player.gd
+++ game_objects/platformer/player.gd
@@ -1,13 +1,16 @@
 extends Control
 
+## Project folder path
+@export var project_path: String = ""
+## Project scene name
 @export var scene_name: String = ""
+## Reference to the canvas node
+@onready var canvas_ui: Control = $CanvasUI
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	if project_path.strip_edges(true, true) != "":
+		if ProjectManager.open_project(project_path):
+			canvas_ui.scene_name = scene_name + Constants.scene_extension
+			canvas_ui.populate_scene_nodes()
